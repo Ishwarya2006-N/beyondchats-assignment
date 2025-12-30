3ff08414dd0fba09d8075eb62e9f7e96c8f15dc6
Ishwarya2006-N <nishwarya11@gmail.com>
2025-12-30 20:26:45 +0530

    chore: use production backend API in frontend

--- frontend/beyondchats/src/api/articlesApi.js
+++ frontend/beyondchats/src/api/articlesApi.js
@@ -1,4 +1,9 @@
+const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://beyondchats-assignment-1-3e5n.onrender.com";
+
 export const fetchArticles = async () => {
-  const res = await fetch("http://localhost:5000/articles");
+  const res = await fetch(`${API_BASE_URL}/articles`);
   return res.json();
 };
